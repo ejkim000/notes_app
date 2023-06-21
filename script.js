796feb39371e030ef166d9f4b3965a2771c437af
ejkim000 <developer.ejk@gmail.com>
2023-06-21 12:37:31 -0400

    edit toggle btn

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addBtn = document.getElementById("add");
-addBtn.addEventListener('click', () => addNewNote('Add new note'));
+addBtn.addEventListener('click', () => addNewNote());
 
-function addNewNote() {
+function addNewNote(text = '') {
     const note = document.createElement('div');
     note.classList.add('note');
 
@@ -18,15 +18,28 @@ function addNewNote() {
             </button>
         </div>
 
-        <div class="hidden"></div>
-        <textarea></textarea>
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
 
     // instead of document, use note
     const deleteBtn = note.querySelector(".delete");
+    const editBtn= note.querySelector(".edit");
+    const main= note.querySelector(".main");
+    const textArea= note.querySelector("textarea");
+
+    textArea.value = text;
+    main.innerHTML = text;
+
     deleteBtn.addEventListener("click", ()=>{
         note.remove();
     });
+
+    editBtn.addEventListener("click", ()=>{
+        main.classList.toggle("hidden");
+        textArea.classList.toggle("hidden");
+    })
+
 
     document.body.appendChild(note);
 }
